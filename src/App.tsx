c31bd331d519692ffc68be06c684dcbfe7fccbb5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-26 00:35:21 -0300

    chore: add home auth page into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Button text="Attribute by props"/>
-      <Button>Children by props</Button>
-      <Button />
-      <Button />
+      <Home />
     </div>
   );
 }
