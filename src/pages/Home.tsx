a0dbe810da759318774acd0569ae5ba9ada158c5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-26 00:36:06 -0300

    chore: add scss of home auth page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,34 @@
-import illustrationImg from '../assets/images/illustration.svg';
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from '../assets/images/illustration.svg';
 
 export function Home() {
     return (
-        <>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustration of questions &amp; answers" />
                 <strong>Create a Q&amp;A room on-live</strong>
                 <p>Answer you audience in real-time</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Google's logo" />
                         Create your room with Google
                     </button>
-                    <div>Or enter in a room</div>
+                    <div className="separator">Or enter in a room</div>
                     <form action="">
                         <input 
                             type="text"
                             placeholder="Type room's code"
                         />
-                        <button type="submit">Enter room</button>
+                        <Button type="submit">Enter room</Button>
                     </form>
                 </div>
             </main>
-        </>
+        </div>
     )
 }
