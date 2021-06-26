3266382f1457f4529953a3a82f8f2eb20e98cce1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-26 01:51:20 -0300

    chore: recover auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { BrowserRouter, Route } from "react-router-dom";
-import { createContext, useState } from "react"
 import { auth, firebase } from './services/firebase'
+import { BrowserRouter, Route } from "react-router-dom";
+import { createContext, useState, useEffect } from "react"
 
 type UserType = {
   id: string;
@@ -19,6 +19,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [ user, setUser ] = useState<UserType>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google Account");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        });
+      }
+    })
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
