a8a193215ae6f5355aa5539eab9a4ea7d16f6577
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-26 01:18:29 -0300

    feat: add auth with firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,18 @@ import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
+import { auth, firebase } from '../services/firebase'
 
 export function Home() {
     const history = useHistory();
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new');
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+            history.push('/rooms/new');
+        });
+
     }
 
     return (
@@ -22,7 +28,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Google's logo" />
                         Create your room with Google
                     </button>
