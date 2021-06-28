3d2330771cea0ee92558a43c2cfee9e545afa579
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-27 21:20:57 -0300

    feat: add create new question flow

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,22 +1,55 @@
-import { useParams } from 'react-router-dom';
-import logoImg from '../assets/images/logo.svg';
-import { Button } from '../components/Button';
-import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss';
+import { useAUth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Button } from '../components/Button';
+import logoImg from '../assets/images/logo.svg';
+import { database } from '../services/firebase';
+import { RoomCode } from '../components/RoomCode';
 
 type RoomParams = {
     id: string;
 }
 
 export function Room() {
+    const { user } = useAUth();
     const params = useParams<RoomParams>();
+    const roomId = params.id;
+    const [ newQuestion, setNewQuestion ] = useState('');
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if (newQuestion.trim() === '') {
+            return;
+        }
+
+        if (!user) {
+            // TODO replace with toast (https://react-hot-toast.com/)
+            throw new Error('You must be logged in.');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar
+            },
+            isHighlighted: false,
+            isAnswered: false
+        }
+
+        database.ref(`rooms/${roomId}/questions`).push(question);
+
+        setNewQuestion('');
+    }
 
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={params.id}/>
+                    <RoomCode code={roomId}/>
                 </div> 
             </header>
 
@@ -26,13 +59,22 @@ export function Room() {
                     <span>4 questions</span>
                 </div>
 
-                <form>
+                <form onSubmit={handleSendQuestion}>
                     <textarea
                         placeholder="What is your question?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
                     />
                     <div className="form-footer">
-                        <span>To make a question, <button>login first</button>.</span>
-                        <Button type="submit">Send question</Button>
+                        { user ? (
+                            <div className="user-info">
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                        ) : (
+                            <span>To make a question, <button>login first</button>.</span>
+                        ) }
+                        <Button type="submit" disabled={!user}>Send question</Button>
                     </div>
                 </form>
             </main>
