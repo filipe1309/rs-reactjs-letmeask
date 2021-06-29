5e030628d426ed7f6b8b3751a9936df8b4f01a83
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-28 22:37:43 -0300

    feat: add remove question flow

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,20 +1,27 @@
 import '../styles/room.scss';
 import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 import { RoomCode } from '../components/RoomCode';
 import { useParams } from 'react-router-dom';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
 
 type RoomParams = {
     id: string;
 }
 
-
 export function AdminRoom() {
     const params = useParams<RoomParams>();
     const roomId = params.id;
     const { questions, title } =  useRoom(roomId);
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Are you sure you want to delete?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return (
         <div id="page-room">
@@ -41,7 +48,14 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remove question" />
+                                </button>
+                            </Question>
                         )
                     })}
                 </div>
